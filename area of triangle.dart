fc0eed749b9cfdeff641456b41d524c447b596da
Nikiashra <154734662+Nikiashra@users.noreply.github.com>
2023-12-28 01:19:54 -0800

    Update area of triangle.dart

--- area of triangle.dart
+++ area of triangle.dart
@@ -1,12 +1,13 @@
 void main()
 {
 
- // area of triangle;
+ print("*****area of triangle*****");
 
   int b=10;
   int h=15;
 
   var area = 1/2*(b*h);
 
-  print("area of triangle is = $area");
+  print("\narea of triangle is = $area");
+  print("==============================");
 }
