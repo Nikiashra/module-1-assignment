fe416a2c4c003c46006a508708d7ec9e636418a3
Nikiashra <154734662+Nikiashra@users.noreply.github.com>
2023-12-27 21:30:27 -0800

    daughter information delete

--- Information.dart
+++ Information.dart
@@ -2,7 +2,5 @@ void main()
 {
   print("My name is Niki Ashra");
   print("birthdate is 25/01/1987");
-  print("I am 35 years old");
   print("I stay at Rajkot");
-  print("My daughter name is aarvi");
 }
