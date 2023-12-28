d0e4e84d72a1f85fc3a11c60e3ba7fa01cfad87e
Nikiashra <154734662+Nikiashra@users.noreply.github.com>
2023-12-27 21:36:51 -0800

    add, sub, mul, div, mod

---
+++ arithmetic solution.dart
@@ -0,0 +1,21 @@
+  void main()
+
+    {
+      int a =55;
+      int b =66;
+
+      int sum = a+b;
+      print (sum);
+
+      int sub = a-b;
+      print(sub);
+
+      int mul =a*b;
+      print(mul);
+
+      double div =a/b;
+      print(div);
+
+      int mod =a%b;
+      print(mod);
+     }
