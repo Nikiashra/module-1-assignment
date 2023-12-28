8675372eb07cdf1e31f157fae18a8f04b687d31d
Nikiashra <154734662+Nikiashra@users.noreply.github.com>
2023-12-27 23:45:17 -0800

    area of square

---
+++ area of square.dart
@@ -0,0 +1,41 @@
+void main()
+  /*{
+  int s = 10;
+  var area = s*s;
+
+
+  print("area of square is=$area");
+}
+
+{
+  int s =6;
+  var area =6*s;
+
+  print("cube of number is=$area");
+}
+{
+  double PI = 6.18;
+  int r = 6;
+
+  double area= PI*r*r;
+  print("Area of circle is=$area");
+}
+{
+ double s =3.50;
+
+ double area=6*s;
+ print("cube of number is =$area");
+
+}*/
+{
+  int p=50000;
+  int t=5;
+  int r=12;
+
+  var SI=(p*t*r/100) ;
+  print("simple intrest =$SI");
+}
+
+
+
+
