35e2cdb73e5c8e4cbf99d029ccd5d2324353104c
Nikiashra <154734662+Nikiashra@users.noreply.github.com>
2023-12-28 01:14:22 -0800

    Update Information

--- Information.dart
+++ Information.dart
@@ -1,9 +1,9 @@
+void main()
 {
   print("*****Information*****");
   print("\nMy name is Niki Ashra");
   print("birthdate is 25/01/1987");
   print("I am 35 years old");
   print("I stay at Rajkot");
-  print("My daughter name is aarvi");
   print("\n===============================");
 }
