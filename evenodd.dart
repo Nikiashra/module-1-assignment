2d04cd997ab55d92366ffad1ea510f6faafc1b43
Nikiashra <154734662+Nikiashra@users.noreply.github.com>
2024-01-15 19:12:04 -0800

    Add files via upload

---
+++ evenodd.dart
@@ -0,0 +1,13 @@
+void main()
+{
+  int number=18;
+
+      if(number % 2==0)
+        {
+          print("The number is even");
+        }
+      else
+        {
+          print("The number is odd");
+        }
+}
