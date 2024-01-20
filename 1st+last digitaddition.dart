61d6c3e792933efd507e6b32666668f195944853
Nikiashra <154734662+Nikiashra@users.noreply.github.com>
2024-01-19 18:34:50 -0800

    Update 1st+last digitaddition.dart

--- 1st+last digitaddition.dart
+++ 1st+last digitaddition.dart
@@ -2,9 +2,10 @@ import 'dart:io';
 
 void main()
 {
-  int l=0;
-  int f;
-  int sum=0;
+  double l=0;
+  double f;
+  double sum=0;
+
 
   print("Enter any Number");
   double num = double.parse(stdin.readLineSync().toString());
@@ -12,11 +13,11 @@ void main()
 
   while(num >= 10)
   {
-    l += num % 10;//find last digit
-    num = num / 10;//find first digit dividing by 10
+    l += num % 10; //find last digit
+    num = num / 10; //find first digit dividing by 10
+    f = num;
+    sum = l + num;
   }
-      f = num;
-      sum = l+f;
   
   {
    print("$sum");
