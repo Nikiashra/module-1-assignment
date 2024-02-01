ac57c07601182123714b84f1ee4ade6087246185
Nikiashra <154734662+Nikiashra@users.noreply.github.com>
2024-01-31 19:59:23 -0800

    Update revers.number.dart

--- test1/revers.number.dart
+++ test1/revers.number.dart
@@ -3,14 +3,14 @@ import 'dart:io';
 void main()
 {
   print("Enter any number");
-  double num= double.parse(stdin.readLineSync().toString());
+  int num= int.parse(stdin.readLineSync().toString());
 
-  double revers = 0;
+  int revers = 0;
 
   while(num>0)
     {
       revers = (revers*10)+num%10;
-      num = num / 10;
+      num = (num / 10).floor();
     }
 
     print("$revers");
